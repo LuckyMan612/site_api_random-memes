dc69b9930bc98dd65aba0842a6b9491cad64de96
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-24 12:49:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 const memes = [
-  "https://raw.githubusercontent.com/username/repo/img/meme1.jpg",
-  "https://raw.githubusercontent.com/username/repo/img/meme2.jpg",
-  "https://raw.githubusercontent.com/username/repo/img/meme3.jpg",
-  // ...dodaj kolejne linki do zdjęć
+  "https://i.pinimg.com/originals/61/2b/ff/612bff8412e03dc3c84ea5aabe743d51.jpg",
+  "https://i.pinimg.com/736x/5d/e5/c9/5de5c9e6e7fecfdca2c8a0a0a2a2f1f9.jpg",
+  "https://i.pinimg.com/736x/d8/b6/c1/d8b6c1e6f8d6c3f6a3e6baafd0d8afb1.jpg"
 ];
+const randomMeme = memes[Math.floor(Math.random() * memes.length)];
 
-function getRandomMeme() {
-  const randomIndex = Math.floor(Math.random() * memes.length);
-  return memes[randomIndex];
+// Strona z zdjęciem mema
+const memeImg = document.getElementById("meme-img");
+if (memeImg) {
+  memeImg.src = randomMeme;
+} else {
+  console.log("Element o id 'meme-img' nie został znaleziony na stronie");
 }
 
-const memeImg = document.getElementById("meme-img");
-memeImg.src = getRandomMeme();
-
-const API_URL = "https://raw.githubusercontent.com/LuckyMan612/site_api_random-memes/main/api.json";
-const newData = { "meme": { "link": getRandomMeme() } };
-
-fetch(API_URL, {
-  method: "PATCH",
-  body: JSON.stringify(newData),
-  headers: { "Content-Type": "application/json" }
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log("API updated successfully", data);
-  })
-  .catch(error => {
-    console.error("Error updating API: ", error);
-  });
+// Strona z linkiem do mema w formacie JSON
+const memeLink = document.getElementById("meme-link");
+if (memeLink) {
+  memeLink.innerHTML = JSON.stringify({ link: randomMeme });
+} else {
+  console.log("Element o id 'meme-link' nie został znaleziony na stronie");
+}
